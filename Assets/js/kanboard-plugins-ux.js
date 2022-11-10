19f8f69fbb3554eba061abc0dcb48285e5ae8f87
aljawaid <jawaid_imran@hotmail.com>
2022-11-10 20:15:44 +0000

    FIX: #8 Plugin Filter Causes JS Errors on Other Pages

--- Assets/js/kanboard-plugins-ux.js
+++ Assets/js/kanboard-plugins-ux.js
@@ -1,22 +1,14 @@
 // KANBOARD PLUGIN ASSET FILE
 
-   document.querySelector(".search-input") .addEventListener("keyup", searchFunction);
-   function searchFunction() {
-      var input, filter, table, tr, td, i, txtValue;
-      input = document.querySelector(".search-input");
-      filter = input.value.toUpperCase();
-      table = document.querySelector(".installed-plugins");
-      tr = table.getElementsByTagName("tr");
-      for (i = 0; i < tr.length; i++) {
-         td = tr[i].getElementsByTagName("td")[0];
-         if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-               tr[i].style.display = "";
-            } else {
-               tr[i].style.display = "none";
-            }
-         }
-      }
-   }
+$(document).ready(function(){
+  $("#InstalledPluginsFilterInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#InstalledPluginsTable tbody").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+
+
+
 
