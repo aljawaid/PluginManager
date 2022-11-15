c47cda2b08d14e0d27fe787977992edeb069797f
aljawaid <jawaid_imran@hotmail.com>
2022-11-15 03:49:58 +0000

    Add Installed Count on Directory Page

--- Template/plugin/directory.php
+++ Template/plugin/directory.php
@@ -17,39 +17,40 @@
     <p class="alert"><?= t('There is no plugin available.') ?></p>
 <?php else: ?>
     <?php usort($available_plugins, fn ($a, $b) => strtolower($a['title']) <=> strtolower($b['title'])); ?>
-    <?php $availableCount = count($available_plugins) ?>
-        <div class="directory-info-wrapper">
-            <table id="DirectoryInfo" class="directory-info">
-                <thead class="">
-                    <tr>
-                        <th scope="col" class=""><?= t('Available Plugins') ?></th>
-                        <th scope="col" colspan="2" class="text-center"><?= t('Directory Source') ?></th>
-                        <th scope="col" class=""><?= t('Your Application Version') ?></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td scope="row" class="available-count pp-green">&#10004; <?= $availableCount ?></td>
-                        <td class="plugin-dir-view">
-                            <?php $dirURL = 'https://kanboard.org/plugins.html'; ?>
-                            <?php if (!defined(PLUGIN_API_URL)): ?>
-                                <a href="<?= $dirURL ?>" target="_blank" title="<?= t('Opens in a new window') ?> &#8663; " rel="noopener noreferrer">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-card-list" viewBox="0 0 16 16">
-                                        <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
-                                        <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
-                                    </svg>
-                                    <?= t('View Official Plugin Directory') ?>
-                                </a>
-                            <?php else: ?>
-                                <?= t('Custom Directory') ?>
-                            <?php endif ?>
-                        </td>
-                        <td class="plugin-dir-url"><?= PLUGIN_API_URL ?></td>
-                        <td class="kb-app-version"><?= APP_VERSION ?></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+    <div class="directory-info-wrapper">
+        <table id="DirectoryInfo" class="directory-info">
+            <thead class="">
+                <tr class="">
+                    <th scope="col" class=""><?= t('Available Plugins') ?></th>
+                    <th scope="col" colspan="2" class="text-center"><?= t('Directory Source') ?></th>
+                    <th scope="col" class=""><?= t('Your Application Version') ?></th>
+                    <th scope="col" class=""><?= t('Currently Installed') ?></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="">
+                    <td scope="row" class="available-count pp-green">&#10004; <?= count($available_plugins) ?></td>
+                    <td class="plugin-dir-view">
+                        <?php $dirURL = 'https://kanboard.org/plugins.html'; ?>
+                        <?php if (!defined(PLUGIN_API_URL)): ?>
+                            <a href="<?= $dirURL ?>" target="_blank" title="<?= t('Opens in a new window') ?> &#8663; " rel="noopener noreferrer">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-card-list" viewBox="0 0 16 16">
+                                    <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
+                                    <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
+                                </svg>
+                                <?= t('View Official Plugin Directory') ?>
+                            </a>
+                        <?php else: ?>
+                            <?= t('Custom Directory') ?>
+                        <?php endif ?>
+                    </td>
+                    <td class="plugin-dir-url"><?= PLUGIN_API_URL ?></td>
+                    <td class="kb-app-version"><?= APP_VERSION ?></td>
+                    <td scope="row" class="total-count"><?= count($installed_plugins) ?></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
     <form class="plugin-available-filter">
         <label for="AvailablePluginsFilterInput">
             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-filter-square" viewBox="0 0 16 16">
