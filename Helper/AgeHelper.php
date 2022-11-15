e90cfccb30de6768408b959567b5d7502ce4a6b0
aljawaid <jawaid_imran@hotmail.com>
2022-11-15 07:05:54 +0000

    FIX: Untranslated Strings

--- Helper/AgeHelper.php
+++ Helper/AgeHelper.php
@@ -63,77 +63,77 @@ class AgeHelper extends Base
         } elseif ($diff < 86400) {
             return t('yesterday');
         } elseif ($diff < (86400 * 3)) {
-            return 'a few days ago';
+            return t('a few days ago');
         } elseif ($diff < (86400 * 5)) {
-            return 'less than a week ago';
+            return t('less than a week ago');
         } elseif ($diff < (86400 * 7)) {
-            return 'about a week ago';
+            return t('about a week ago');
         } elseif ($diff < (86400 * 9)) {
-            return 'over a week ago';
+            return t('over a week ago');
         } elseif ($diff < (86400 * 10)) {
-            return 'about 10 days ago';
+            return t('about 10 days ago');
         } elseif ($diff < (86400 * 14)) {
-            return 'about 2 weeks ago';
+            return t('about 2 weeks ago');
         } elseif ($diff < (86400 * 16)) {
-            return 'just over 2 weeks ago';
+            return t('just over 2 weeks ago');
         } elseif ($diff < (86400 * 19)) {
-            return 'nearly 3 weeks ago';
+            return t('nearly 3 weeks ago');
         } elseif ($diff < (86400 * 21)) {
-            return 'about 3 weeks ago';
+            return t('about 3 weeks ago');
         } elseif ($diff < (86400 * 27)) {
-            return 'over 3 weeks ago';
+            return t('over 3 weeks ago');
         } elseif ($diff < (86400 * 31)) {
-            return 'about a month ago';
+            return t('about a month ago');
         } elseif ($diff < (86400 * 35)) {
-            return 'just over a month ago';
+            return t('just over a month ago');
         } elseif ($diff < (86400 * 59)) {
-            return 'over a month ago';
+            return t('over a month ago');
         } elseif ($diff < (86400 * 61)) {
-            return 'about 2 months ago';
+            return t('about 2 months ago');
         } elseif ($diff < (86400 * 88)) {
-            return 'over 2 months ago';
+            return t('over 2 months ago');
         } elseif ($diff < (86400 * 91)) {
-            return 'about 3 months ago';
+            return t('about 3 months ago');
         } elseif ($diff < (86400 * 97)) {
-            return 'just over 3 months ago';
+            return t('just over 3 months ago');
         } elseif ($diff < (86400 * 118)) {
-            return 'over 3 months ago';
+            return t('over 3 months ago');
         } elseif ($diff < (86400 * 120)) {
-            return 'about 4 months ago';
+            return t('about 4 months ago');
         } elseif ($diff < (86400 * 149)) {
-            return 'over 4 months ago';
+            return t('over 4 months ago');
         } elseif ($diff < (86400 * 150)) {
-            return 'about 5 months ago';
+            return t('about 5 months ago');
         } elseif ($diff < (86400 * 159)) {
-            return 'just over 5 months ago';
+            return t('just over 5 months ago');
         } elseif ($diff < (86400 * 180)) {
-            return 'about 6 months ago';
+            return t('about 6 months ago');
         } elseif ($diff < (86400 * 200)) {
-            return 'just over 6 months ago';
+            return t('just over 6 months ago');
         } elseif ($diff < (86400 * 363)) {
-            return 'over 6 months ago';
+            return t('over 6 months ago');
         } elseif ($diff < (86400 * 365)) {
-            return 'a year ago';
+            return t('a year ago');
         } elseif ($diff < (86400 * 385)) {
-            return 'just over a year ago';
+            return t('just over a year ago');
         } elseif ($diff < (86400 * 729)) {
-            return 'over a year ago';
+            return t('over a year ago');
         } elseif ($diff < (86400 * 740)) {
-            return 'about 2 years ago';
+            return t('about 2 years ago');
         } elseif ($diff < (86400 * 1094)) {
-            return 'over 2 years ago';
+            return t('over 2 years ago');
         } elseif ($diff < (86400 * 1105)) {
-            return 'about 3 years ago';
+            return t('about 3 years ago');
         } elseif ($diff < (86400 * 1400)) {
-            return 'over 3 years ago';
+            return t('over 3 years ago');
         } elseif ($diff < (86400 * 1470)) {
-            return 'about 4 years ago';
+            return t('about 4 years ago');
         } elseif ($diff < (86400 * 1800)) {
-            return 'over 4 years ago';
+            return t('over 4 years ago');
         } elseif ($diff <= (86400 * 1828)) {
-            return 'about 5 years ago';
+            return t('about 5 years ago');
         } elseif ($diff > (86400 * 1828)) {
-            return 'over 5 years ago';
+            return t('over 5 years ago');
         }
         return 'If you see this return, I have made a mistake';;
     }
