fbfa44bec89a99cee8e0938ae1127fa5ad088c4b
aljawaid <jawaid_imran@hotmail.com>
2022-11-15 06:01:36 +0000

    NEW: Revamp Plugin Side Menu
    - Completes #5 Revamp Plugin Side Menu
    - Corrects ab7c85e

--- Template/plugin/sidebar.php
+++ Template/plugin/sidebar.php
@@ -1,25 +1,13 @@
 <div id="PluginSideBar" class="sidebar">
     <ul id="PluginMenu" class="plugin-menu">
         <li <?= $this->app->checkMenuSelection('PluginController', 'show') ?>>
-            <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="bi bi-plugin" viewBox="0 0 16 16">
-                <title><?= t('Plugin Manager') ?></title>
-                <path fill-rule="evenodd" d="M1 8a7 7 0 1 1 2.898 5.673c-.167-.121-.216-.406-.002-.62l1.8-1.8a3.5 3.5 0 0 0 4.572-.328l1.414-1.415a.5.5 0 0 0 0-.707l-.707-.707 1.559-1.563a.5.5 0 1 0-.708-.706l-1.559 1.562-1.414-1.414 1.56-1.562a.5.5 0 1 0-.707-.706l-1.56 1.56-.707-.706a.5.5 0 0 0-.707 0L5.318 5.975a3.5 3.5 0 0 0-.328 4.571l-1.8 1.8c-.58.58-.62 1.6.121 2.137A8 8 0 1 0 0 8a.5.5 0 0 0 1 0Z"/>
-            </svg>
-            <?= $this->url->link(t('Plugin Manager'), 'PluginController', 'show', array(), false, 'plugin-menu-item') ?>
+            <?= $this->url->link(t('Plugin Manager'), 'PluginController', 'show', array(), false, 'plugin-manager-item') ?>
         </li>
         <li <?= $this->app->checkMenuSelection('PluginController', 'directory') ?>>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tools" viewBox="0 0 16 16">
-                <title><?= t('Plugin Directory') ?></title>
-                <path d="M1 0 0 1l2.2 3.081a1 1 0 0 0 .815.419h.07a1 1 0 0 1 .708.293l2.675 2.675-2.617 2.654A3.003 3.003 0 0 0 0 13a3 3 0 1 0 5.878-.851l2.654-2.617.968.968-.305.914a1 1 0 0 0 .242 1.023l3.27 3.27a.997.997 0 0 0 1.414 0l1.586-1.586a.997.997 0 0 0 0-1.414l-3.27-3.27a1 1 0 0 0-1.023-.242L10.5 9.5l-.96-.96 2.68-2.643A3.005 3.005 0 0 0 16 3c0-.269-.035-.53-.102-.777l-2.14 2.141L12 4l-.364-1.757L13.777.102a3 3 0 0 0-3.675 3.68L7.462 6.46 4.793 3.793a1 1 0 0 1-.293-.707v-.071a1 1 0 0 0-.419-.814L1 0Zm9.646 10.646a.5.5 0 0 1 .708 0l2.914 2.915a.5.5 0 0 1-.707.707l-2.915-2.914a.5.5 0 0 1 0-.708ZM3 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026L3 11Z"/>
-            </svg>
-            <?= $this->url->link(t('Plugin Directory'), 'PluginController', 'directory', array(), false, 'plugin-menu-item') ?>
+            <?= $this->url->link(t('Plugin Directory'), 'PluginController', 'directory', array(), false, 'plugin-directory-item') ?>
         </li>
         <li <?= $this->app->checkMenuSelection('PluginManagerController', 'show') ?>>
-            <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor" class="bi bi-cone-striped" viewBox="0 0 16 16">
-                <title><?= t('Plugin Problems') ?></title>
-                <path d="m9.97 4.88.953 3.811C10.159 8.878 9.14 9 8 9c-1.14 0-2.158-.122-2.923-.309L6.03 4.88C6.635 4.957 7.3 5 8 5s1.365-.043 1.97-.12zm-.245-.978L8.97.88C8.718-.13 7.282-.13 7.03.88L6.275 3.9C6.8 3.965 7.382 4 8 4c.618 0 1.2-.036 1.725-.098zm4.396 8.613a.5.5 0 0 1 .037.96l-6 2a.5.5 0 0 1-.316 0l-6-2a.5.5 0 0 1 .037-.96l2.391-.598.565-2.257c.862.212 1.964.339 3.165.339s2.303-.127 3.165-.339l.565 2.257 2.391.598z"/>
-            </svg>
-            <?= $this->url->link(t('Plugin Problems'), 'PluginManagerController', 'show', array('plugin' => 'PluginManager'), false, 'plugin-menu-item') ?>
+            <?= $this->url->link(t('Plugin Problems'), 'PluginManagerController', 'show', array('plugin' => 'PluginManager'), false, 'plugin-problems-item') ?>
         </li>
     </ul>
 </div>
