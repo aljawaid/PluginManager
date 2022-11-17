344173982a140f603180b544c79c51cdbb8df594
aljawaid <jawaid_imran@hotmail.com>
2022-11-17 22:18:58 +0000

    FIX: Correct Text to Include Models
    - Fixes #12 Add New Plugin Properties to All Plugins in The Plugins Directory
    - WIP #20

--- Template/plugin/directory.php
+++ Template/plugin/directory.php
@@ -350,14 +350,14 @@
                     <?php if ($plugin['has_overrides']): ?>
                         <!-- OVERRIDES IS TRUE -->
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="btn-action bi bi-file-earmark-plus has-overrides" fill="currentColor" viewBox="0 0 16 16">
-                            <title><?= t('This plugin overrides certain templates') ?></title>
+                            <title><?= t('This plugin overrides certain templates or models') ?></title>
                             <path d="M8 6.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 .5-.5z"/>
                             <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z"/>
                         </svg>
                     <?php else: ?>
                         <!-- OVERRIDES IS FALSE -->
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="btn-action bi bi-file-earmark-check xx-has-overrides" fill="currentColor" viewBox="0 0 16 16">
-                            <title><?= t('This plugin does not override any templates') ?></title>
+                            <title><?= t('This plugin does not override any templates or models') ?></title>
                             <path d="M10.854 7.854a.5.5 0 0 0-.708-.708L7.5 9.793 6.354 8.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"/>
                             <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
                         </svg>
@@ -365,7 +365,7 @@
                 <?php else: ?>
                     <!-- NOT SPECIFIED / ASSUME FALSE -->
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="btn-action bi bi-file-earmark-diff x-has-overrides" fill="currentColor" viewBox="0 0 16 16">
-                        <title><?= t('This plugin has not specified any template overrides') ?></title>
+                        <title><?= t('This plugin has not specified any template or model overrides') ?></title>
                         <path d="M8 5a.5.5 0 0 1 .5.5V7H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V8H6a.5.5 0 0 1 0-1h1.5V5.5A.5.5 0 0 1 8 5zm-2.5 6.5A.5.5 0 0 1 6 11h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5z"/>
                         <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
                     </svg>
