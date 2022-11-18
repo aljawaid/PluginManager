6c802ce0b04df848a9daea6392259d0a8bce2d0a
aljawaid <jawaid_imran@hotmail.com>
2022-11-18 00:51:59 +0000

    Update Translations
    WIP #19

--- Template/plugin/directory.php
+++ Template/plugin/directory.php
@@ -9,7 +9,7 @@
 
 <?php if (! $is_configured): ?>
 <p class="alert alert-error">
-    <?= t('Your Kanboard instance is not configured to install plugins from the user interface.') ?>
+    <?= t('Your application instance is not configured to install plugins from the user interface.') ?>
 </p>
 <?php endif ?>
 
@@ -38,7 +38,7 @@
                                     <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
                                     <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
                                 </svg>
-                                <?= t('View Official Plugin Directory') ?>
+                                <?= t('Official Plugin Directory') ?>
                             </a>
                         <?php else: ?>
                             <?= t('Custom Directory') ?>
