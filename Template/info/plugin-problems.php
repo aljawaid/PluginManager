dff78c19266e180060f2275b2c4d5c83d4fd7544
aljawaid <jawaid_imran@hotmail.com>
2022-11-20 00:37:37 +0000

    Better Wording

--- Template/info/plugin-problems.php
+++ Template/info/plugin-problems.php
@@ -80,7 +80,7 @@
         </div>
         <p class="">
             <?php if (file_exists('plugins/KanboardSupport')): ?>
-                <?= t('All information related to your setup can be easily found in the Support menu.') ?>
+                <?= t('All information related to your setup can be easily found in the menu.') ?>
             <?php endif ?>
         </p>
     </section>
@@ -142,7 +142,7 @@
                         <path d="m10.273 2.513-.921-.944.715-.698.622.637.89-.011a2.89 2.89 0 0 1 2.924 2.924l-.01.89.636.622a2.89 2.89 0 0 1 0 4.134l-.637.622.011.89a2.89 2.89 0 0 1-2.924 2.924l-.89-.01-.622.636a2.89 2.89 0 0 1-4.134 0l-.622-.637-.89.011a2.89 2.89 0 0 1-2.924-2.924l.01-.89-.636-.622a2.89 2.89 0 0 1 0-4.134l.637-.622-.011-.89a2.89 2.89 0 0 1 2.924-2.924l.89.01.622-.636a2.89 2.89 0 0 1 4.134 0l-.715.698a1.89 1.89 0 0 0-2.704 0l-.92.944-1.32-.016a1.89 1.89 0 0 0-1.911 1.912l.016 1.318-.944.921a1.89 1.89 0 0 0 0 2.704l.944.92-.016 1.32a1.89 1.89 0 0 0 1.912 1.911l1.318-.016.921.944a1.89 1.89 0 0 0 2.704 0l.92-.944 1.32.016a1.89 1.89 0 0 0 1.911-1.912l-.016-1.318.944-.921a1.89 1.89 0 0 0 0-2.704l-.944-.92.016-1.32a1.89 1.89 0 0 0-1.912-1.911l-1.318.016z"/>
                         <path d="M7.001 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0z"/>
                     </svg>
-                    <?= t('Delete the folder (most likely the last updated one will be the offending plugin).') ?>
+                    <?= t('Delete the folder for the plugin. It is highly likely that the last updated folder will be the offending plugin).') ?>
                 </li>
             </ul>
         </div>
