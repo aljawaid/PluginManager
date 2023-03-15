972150a3918f24c39902cdb07e7daf7be9810245
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-03-15 20:11:53 +0000

    NEW: Add Manual Plugin Count

--- Template/plugin/manual-plugins.php
+++ Template/plugin/manual-plugins.php
@@ -5,7 +5,7 @@
             <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/>
             <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/>
         </svg>
-        <?= t('Manual Plugins') ?>
+        <?= t('Manual Plugins') ?> <span class="manual-plugin-count"><?= count($this->helper->pluginManagerHelper->getAllPlugins()) ?></span>
     </h2>
 
     <?php if (!empty($this->helper->pluginManagerHelper->getAllPlugins())): ?>
@@ -258,9 +258,6 @@
                         </svg>
                      <?php endif ?>
                 </td>
-
-
-
                 <td class="available-plugin-status text-center">
                     <?php if ($plugin['download']): ?>
                         <a href="<?= ($plugin['download']) ?>" class="download-archive" title="<?= t('Download file') ?>"><?= t('Download Plugin') ?></a>
@@ -268,10 +265,6 @@
                         <div class="cross">&#10008;</div> <?= t('Not available') ?>
                     <?php endif ?>
                 </td>
-
-
-
-
             </tr>
             <tr class="">
                 <td class="available-plugin-description" colspan="5">
