d6d38b5551118b34e47f36590a89a8738a2420ac
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-04-07 07:29:32 +0100

    NEW: Add Unique Table IDs per Plugin

--- Template/plugin/directory.php
+++ Template/plugin/directory.php
@@ -175,7 +175,7 @@
             <a id="PluginBottom" href="#PluginTop" title="<?= t('Go to the bottom of the page') ?>" class="btn-action"><i class="fa fa-level-down" aria-hidden="true"></i> <?= t('Bottom') ?></a>
         </div>
         <?php foreach ($available_plugins as $plugin): ?>
-        <table class="available-plugins-table">
+        <table id="Plugin-<?= preg_replace('/\s+/', '',($this->text->e($plugin['title']))) ?>" class="available-plugins-table">
             <tr class="">
                 <th class="available-plugins-author" colspan="2">
                     <?php if (isset($plugin['is_type'])): ?>
