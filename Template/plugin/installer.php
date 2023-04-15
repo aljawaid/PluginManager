d12dde556cbab680872dbbb571969bdb0d515bc0
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-04-15 04:07:28 +0100

    Update HTML

--- Template/plugin/installer.php
+++ Template/plugin/installer.php
@@ -1,11 +1,15 @@
-<fieldset class="">
+<fieldset class="manual-install">
     <legend><?= t('Manually Install Plugin') ?></legend>
-    <form method="post" action="<?=$this->url->href('PluginManagerController', 'installPlugin', array('plugin' => 'PluginManager'))?>" enctype="multipart/form-data" autocomplete="on" >
+    <form method="post" class="file-url-form" action="<?=$this->url->href('PluginManagerController', 'installPlugin', array('plugin' => 'PluginManager'))?>" enctype="multipart/form-data" autocomplete="on" >
         <?= $this->form->csrf() ?>
         <?= $this->form->label(t('From File'), 'pluginfile') ?>
         <input type="file" accept="application/zip" name="pluginfile" id="form-pluginfile">
         <?= $this->form->label(t('From URL'), 'pluginfile') ?>
         <input type="url" id="plugin_url" name="plugin_url" placeholder="https://whatever/plugin.zip"/>
+        <section class="install-section">
+        </section>
+        <section class="install-section">
+        </section>
         <div class="form-actions">
             <button type="submit" class="btn btn-blue"><?= t('Install') ?></button>
         </div>
