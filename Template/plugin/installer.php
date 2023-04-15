63d12b3b93a3ac7992ea8480d40b7aa224f85f8a
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-04-15 22:45:36 +0100

    FIX: Duplicate `id` for `input`
    - #44

--- Template/plugin/installer.php
+++ Template/plugin/installer.php
@@ -5,11 +5,11 @@
         <p class=""><?= t('Choose to install from a local archive (.zip) file or a remote location') ?></p>
         <section class="install-section">
             <?= $this->form->label(t('From File'), 'plugin_file') ?>
-            <?= $this->form->input('file', 'plugin_file', array(), array(), array('id="form-pluginfile" accept="application/zip"'), 'plugin-file') ?>
+            <?= $this->form->input('file', 'plugin_file', array(), array(), array('accept="application/zip"'), 'plugin-file') ?>
         </section>
         <section class="install-section">
             <?= $this->form->label(t('From URL'), 'plugin_url') ?>
-            <?= $this->form->input('url', 'plugin_url', array(), array(), array('id="plugin_url" placeholder="https://mydomain.com/plugin.zip"'), 'plugin-url') ?>
+            <?= $this->form->input('url', 'plugin_url', array(), array(), array('placeholder="https://mydomain.com/plugin.zip"'), 'plugin-url') ?>
         </section>
         <div class="form-actions">
             <button type="submit" class="btn install-plugin-btn install-plugin"><i class="fa fa-cloud-download"></i><?= t('Install') ?></button>
