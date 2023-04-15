ed2c618433ac6c8401fa8176ed4aff19c4362e6c
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-04-15 04:08:16 +0100

    Update Form

--- Template/plugin/installer.php
+++ Template/plugin/installer.php
@@ -2,13 +2,14 @@
     <legend><?= t('Manually Install Plugin') ?></legend>
     <form method="post" class="file-url-form" action="<?=$this->url->href('PluginManagerController', 'installPlugin', array('plugin' => 'PluginManager'))?>" enctype="multipart/form-data" autocomplete="on" >
         <?= $this->form->csrf() ?>
-        <?= $this->form->label(t('From File'), 'pluginfile') ?>
-        <input type="file" accept="application/zip" name="pluginfile" id="form-pluginfile">
-        <?= $this->form->label(t('From URL'), 'pluginfile') ?>
-        <input type="url" id="plugin_url" name="plugin_url" placeholder="https://whatever/plugin.zip"/>
+        <p class=""><?= t('Choose to install from a local archive (.zip) file or a remote location') ?></p>
         <section class="install-section">
+            <?= $this->form->label(t('From File'), 'pluginfile') ?>
+            <input type="file" accept="application/zip" class="plugin-file" name="pluginfile" id="form-pluginfile">
         </section>
         <section class="install-section">
+            <?= $this->form->label(t('From URL'), 'plugin_url') ?>
+            <?= $this->form->input('url', 'plugin_url', $values, array(), array('id="plugin_url" placeholder="https://mydomain.com/plugin.zip"'), 'plugin-url') ?>
         </section>
         <div class="form-actions">
             <button type="submit" class="btn btn-blue"><?= t('Install') ?></button>
