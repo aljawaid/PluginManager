c2c9017687f886ff7413ef9aad4af22b2058519f
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-05-15 13:23:34 +0100

    Update sidebar.php
    - #54

--- Template/plugin/sidebar.php
+++ Template/plugin/sidebar.php
@@ -1,57 +1,28 @@
 <div id="PluginSideBar" class="sidebar">
     <ul id="PluginMenu" class="plugin-menu">
         <li <?= $this->app->checkMenuSelection('PluginController', 'show') ?>>
-            <?= $this->url->link('
-                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-plugin" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M1 8a7 7 0 1 1 2.898 5.673c-.167-.121-.216-.406-.002-.62l1.8-1.8a3.5 3.5 0 0 0 4.572-.328l1.414-1.415a.5.5 0 0 0 0-.707l-.707-.707 1.559-1.563a.5.5 0 1 0-.708-.706l-1.559 1.562-1.414-1.414 1.56-1.562a.5.5 0 1 0-.707-.706l-1.56 1.56-.707-.706a.5.5 0 0 0-.707 0L5.318 5.975a3.5 3.5 0 0 0-.328 4.571l-1.8 1.8c-.58.58-.62 1.6.121 2.137A8 8 0 1 0 0 8a.5.5 0 0 0 1 0Z"/>
-                </svg>' . t('Plugin Manager'), 'PluginController', 'show', array(), false, 'plugin-manager-item') ?>
+            <?= $this->url->link('<span class="pm-plugin-icon-grey"></span>' . t('Plugin Manager'), 'PluginController', 'show', array(), false, 'plugin-manager-item') ?>
         </li>
         <li <?= $this->app->checkMenuSelection('PluginController', 'directory') ?>>
-            <?= $this->url->link('
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-tools" viewBox="0 0 16 16">
-                    <path d="M1 0 0 1l2.2 3.081a1 1 0 0 0 .815.419h.07a1 1 0 0 1 .708.293l2.675 2.675-2.617 2.654A3.003 3.003 0 0 0 0 13a3 3 0 1 0 5.878-.851l2.654-2.617.968.968-.305.914a1 1 0 0 0 .242 1.023l3.27 3.27a.997.997 0 0 0 1.414 0l1.586-1.586a.997.997 0 0 0 0-1.414l-3.27-3.27a1 1 0 0 0-1.023-.242L10.5 9.5l-.96-.96 2.68-2.643A3.005 3.005 0 0 0 16 3c0-.269-.035-.53-.102-.777l-2.14 2.141L12 4l-.364-1.757L13.777.102a3 3 0 0 0-3.675 3.68L7.462 6.46 4.793 3.793a1 1 0 0 1-.293-.707v-.071a1 1 0 0 0-.419-.814L1 0Zm9.646 10.646a.5.5 0 0 1 .708 0l2.914 2.915a.5.5 0 0 1-.707.707l-2.915-2.914a.5.5 0 0 1 0-.708ZM3 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026L3 11Z"/>
-                </svg>' . t('Plugin Directory'), 'PluginController', 'directory', array(), false, 'plugin-directory-item') ?>
+            <?= $this->url->link('<span class="pm-plugin-directory-icon-grey"></span>' . t('Plugin Directory'), 'PluginController', 'directory', array(), false, 'plugin-directory-item') ?>
         </li>
         <li <?= $this->app->checkMenuSelection('PluginManagerController', 'showManualPlugins') ?>>
-            <?= $this->url->link('
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-journal-bookmark" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M6 8V1h1v6.117L8.743 6.07a.5.5 0 0 1 .514 0L11 7.117V1h1v7a.5.5 0 0 1-.757.429L9 7.083 6.757 8.43A.5.5 0 0 1 6 8z"/>
-                    <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/>
-                    <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/>
-                </svg>' . t('Manual Plugins'), 'PluginManagerController', 'showManualPlugins', array('plugin' => 'PluginManager'), false, 'plugin-info-item') ?>
+            <?= $this->url->link('<span class="pm-manual-plugin-icon-grey"></span>' . t('Manual Plugins'), 'PluginManagerController', 'showManualPlugins', array('plugin' => 'PluginManager'), false, 'plugin-info-item') ?>
         </li>
         <li <?= $this->app->checkMenuSelection('PluginManagerController', 'showPluginInfo') ?>>
-            <?= $this->url->link('
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
-                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                    <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-                </svg>' . t('Plugin Info'), 'PluginManagerController', 'showPluginInfo', array('plugin' => 'PluginManager'), false, 'plugin-info-item') ?>
+            <?= $this->url->link('<span class="pm-info-circle-icon"></span>' . t('Plugin Info'), 'PluginManagerController', 'showPluginInfo', array('plugin' => 'PluginManager'), false, 'plugin-info-item') ?>
         </li>
         <li <?= $this->app->checkMenuSelection('PluginManagerController', 'show') ?>>
-            <?= $this->url->link('
-                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-cone-striped" viewBox="0 0 16 16">
-                    <path d="m9.97 4.88.953 3.811C10.159 8.878 9.14 9 8 9c-1.14 0-2.158-.122-2.923-.309L6.03 4.88C6.635 4.957 7.3 5 8 5s1.365-.043 1.97-.12zm-.245-.978L8.97.88C8.718-.13 7.282-.13 7.03.88L6.275 3.9C6.8 3.965 7.382 4 8 4c.618 0 1.2-.036 1.725-.098zm4.396 8.613a.5.5 0 0 1 .037.96l-6 2a.5.5 0 0 1-.316 0l-6-2a.5.5 0 0 1 .037-.96l2.391-.598.565-2.257c.862.212 1.964.339 3.165.339s2.303-.127 3.165-.339l.565 2.257 2.391.598z"/>
-                </svg>' . t('Plugin Problems'), 'PluginManagerController', 'show', array('plugin' => 'PluginManager'), false, 'plugin-problems-item') ?>
+            <?= $this->url->link('<span class="pm-problems-icon-grey"></span>' . t('Plugin Problems'), 'PluginManagerController', 'show', array('plugin' => 'PluginManager'), false, 'plugin-problems-item') ?>
         </li>
         <?php if (file_exists('plugins/KanboardSupport')): ?>
             <li <?= $this->app->checkMenuSelection('TechnicalSupportController', 'show') ?>>
-                <?= $this->url->link('
-                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-journal-code" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M8.646 5.646a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L10.293 8 8.646 6.354a.5.5 0 0 1 0-.708zm-1.292 0a.5.5 0 0 0-.708 0l-2 2a.5.5 0 0 0 0 .708l2 2a.5.5 0 0 0 .708-.708L5.707 8l1.647-1.646a.5.5 0 0 0 0-.708z"/>
-                        <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/>
-                        <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/>
-                    </svg>' . t('Technical Info'), 'TechnicalSupportController', 'show', array('plugin' => 'KanboardSupport'), false, 'plugin-problems-support') ?>
+                <?= $this->url->link('<span class="pm-kanboard-support-icon"></span>' . t('Technical Info'), 'TechnicalSupportController', 'show', array('plugin' => 'KanboardSupport'), false, 'plugin-problems-support') ?>
             </li>
         <?php endif ?>
         <?php if (file_exists('plugins/ContentCleaner')): ?>
             <li class="content-cleaner-menu-link">
-                <?= $this->url->link('
-                    <svg width="24px" height="24px" class="content-cleaner" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-                        <rect fill="currentColor" x="20" y="18" width="6" height="2" transform="translate(46 38) rotate(-180)"/>
-                        <rect fill="currentColor" x="24" y="26" width="6" height="2" transform="translate(54 54) rotate(-180)"/>
-                        <rect fill="currentColor" x="22" y="22" width="6" height="2" transform="translate(50 46) rotate(-180)"/>
-                        <path fill="brown" d="M17.0029,20a4.8952,4.8952,0,0,0-2.4044-4.1729L22,3,20.2691,2,12.6933,15.126A5.6988,5.6988,0,0,0,7.45,16.6289C3.7064,20.24,3.9963,28.6821,4.01,29.04a1,1,0,0,0,1,.96H20.0012a1,1,0,0,0,.6-1.8C17.0615,25.5439,17.0029,20.0537,17.0029,20ZM11.93,16.9971A3.11,3.11,0,0,1,15.0041,20c0,.0381.0019.208.0168.4688L9.1215,17.8452A3.8,3.8,0,0,1,11.93,16.9971ZM15.4494,28A5.2,5.2,0,0,1,14,25H12a6.4993,6.4993,0,0,0,.9684,3H10.7451A16.6166,16.6166,0,0,1,10,24H8a17.3424,17.3424,0,0,0,.6652,4H6c.031-1.8364.29-5.8921,1.8027-8.5527l7.533,3.35A13.0253,13.0253,0,0,0,17.5968,28Z"/>
-                    </svg>' . t('Content Cleaner'), 'ContentCleanerController', 'show', array('plugin' => 'ContentCleaner'), false, 'plugin-content-cleaner-item') ?>
+                <?= $this->url->link('<span class="pm-content-cleaner-icon-grey"></span>' . t('Content Cleaner'), 'ContentCleanerController', 'show', array('plugin' => 'ContentCleaner'), false, 'plugin-content-cleaner-item') ?>
             </li>
         <?php endif ?>
         <?= $this->hook->render('template:plugin:sidebar') ?>
